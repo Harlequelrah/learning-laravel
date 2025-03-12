07b82a309620392fff0b98641956efc3c3a8b74d
Harlequelrah <degbovimax19@gmail.com>
2025-03-12 14:50:36 +0000

    ajout d'un controlleur

--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,29 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
-Route::prefix('/blog')->name('blog.')->group(
+Route::prefix('/blog')->controller(BlogController::class)->name('blog.')->group(
     function () {
 
-        Route::get(
-            "/",
-            function (Request $request) {
-                // $post          = new Post();
-                // $post->title   = 'Mon second article';
-                // $post->slug    = 'mon-second-article';
-                // $post->content = 'Mon contenu';
-                // $post->save();
-                // $post          = new Post();
-                // $post->title   = 'Mon premier article';
-                // $post->slug    = 'mon-premier-article';
-                // $post->content = 'Mon contenu';
-                // $post->save();
-                // return $post;
-                // return Post::paginate(1);
-                // return Post::where('id', '>', '0')->limit(1)->get();
-                // $post1 = Post::find(1);
-                // $post1->title = 'My new title';
-                // $post1->save();
+        Route::get("/",'index')->name('index');
 
-                // $post2 = Post::find(2);
-                // $post2.delete();
-                // return $post1;
-
-                $post3 = Post::create(
-                    [
-                        'title' => 'Harlequin',
-                        'slug' => 'My-Life',
-                        'content' => 'Elrah',
-                    ]
-                );
-                return $post3;
-                // $posts = Post::findOrFail(4);
-                // dd($posts);
-
-                // $posts= Post::all(['id','title']);
-                // dd($posts->first());
-                // return [
-                //     "link" => \route('blog.show', ["slug" => "h1q-22", "id" => 1]),
-                //     "name" => $request->input("name", "nobody"),
-                // ];
-            }
-        );
-    }
-    )->name('index');
-
-    Route::get("/{slug}-{id}", function (string $slug, string $id) {
-        $post = Post::findOrFail($id);
-    return $post
+    Route::get("/{slug}-{id}",'show')
     ->where([
         'slug' => '[a-zA-Z0-9-]+',
         'id'   => '[0-9]+']
     )->name('show');
+
+
 });
+
+
+
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
